a777add01146bfd9a13342a1171f50e98a15441b
jhl <47squared@JHs-MacBook-Pro.local>
2019-09-08 06:44:58 -0700

    Version 1

---
+++ generic-arithmetic.scm
@@ -0,0 +1,294 @@
+(load "help.scm")
+(load "table.scm")
+
+;;;; main procedures
+
+;; simplify:
+(define (simplify radical)
+  (apply-generic 'simplify radical))
+
+
+(define (add x y)
+  (apply-generic 'add x y))
+
+(define (sub x y)
+  (apply-generic 'sub x y))
+
+(define (mul x y)
+  (apply-generic 'mul x y))
+
+(define (div x y)
+  (apply-generic 'div x y))
+
+(define (make-radical coeff rad)
+  ((get 'make-radical 'radical) coeff rad))
+
+(define (make-rat numer denom)
+  ((get 'make 'fraction) numer denom))
+
+(define (install-radical-package)
+  ;; internal procedures
+  
+  (define mul-radicals
+    (lambda (radicalA radicalB)
+      (let ((rad (make-radical (* (coefficient radicalA)
+				  (coefficient radicalB))
+			       (* (radicand radicalA)
+				  (radicand radicalB)))))
+	(simplify rad))))
+
+  ;; div-radicals
+  
+  (define add-radicals
+    (lambda (radicalA radicalB)
+      (operate radicalA radicalB +)))
+
+  (define sub-radicals
+    (lambda (radicalA radicalB)
+      (operate radicalA radicalB -)))
+
+  (define operate
+    (lambda (radicalA radicalB term)
+      (let ((radical1 (simplify radicalA))
+	    (radical2 (simplify radicalB)))
+	(cond ((same-radicand? radical1 radical2)
+	       (make-radical (term (coefficient radical1)
+				     (coefficient radical2))
+			     (radicand radical1)))
+	      ((both-numbers? radical1 radical2)
+	       (term radical1 radical2))
+	      (else (make-addition radical1 radical2))))))
+
+  (define both-numbers?
+    (lambda (x y)
+      (and (number? x) (number? y))))
+
+  ;; This procedure simplifies a given radical -- it is based on the
+  ;; following 3 observations:
+  ;;     -
+  ;;     -
+  ;;     -
+  ;; pair -> number or pair
+  (define simplify
+    (lambda (radical)
+      (define (iter a b max)
+	(cond ((>= a max) (make-radical (* 1 b) max))
+	      ((perfect-square? max) (* b (sqrt max)))
+	      ((= (remainder max a) 0)
+	       (let ((n (/ max a)))
+		 (if (perfect-square? n)
+		     (make-radical (* b (sqrt n)) a)
+		     (iter (+ a 1) b max))))
+	      (else (iter (+ a 1) b max))))
+      (iter 2 (coefficient radical) (radicand radical))))
+
+  (define perfect-square?
+    (lambda (n)
+      (define (iter a b max)
+	(cond ((>= a max) #f)
+	      ((= (* a b) max) #t)
+	      (else (iter (+ a 1) (+ b 1) max))))
+      (iter 1 1 n)))
+  
+  ;; representation of radicals
+  
+  (define make-addition
+    (lambda (radicalA radicalB)
+      (cons radicalA radicalB)))
+  
+  (define make-radical
+    (lambda (coefficient radicand)
+      (cons coefficient radicand)))
+  
+  (define coefficient
+    (lambda (radical)
+      (car radical)))
+  
+  (define radicand
+    (lambda (radical)
+      (cdr radical)))
+  
+  (define same-radicand?
+    (lambda (radicalA radicalB)
+      (and (radical? radicalA)
+	   (radical? radicalB)
+	   (eq? (radicand radicalA) (radicand radicalB)))))
+  
+  (define radical?
+    (lambda (r)
+      (pair? r)))
+
+  ;; interface to the rest of the system
+
+  (define tag
+    (lambda (x) (attach-tag 'radical x)))
+  
+  (put 'simplify '(radical)
+       (lambda (x) (tag (simplify x))))
+  
+  (put 'add '(radical radical)
+       (lambda (rad1 rad2) (tag (add-radicals rad1 rad2))))
+  
+  (put 'sub '(radical radical)
+       (lambda (rad1 rad2) (tag (sub-radicals rad1 rad2))))
+  
+  (put 'mul '(radical radical)
+       (lambda (rad1 rad2) (tag (mul-radicals rad1 rad2))))
+  
+  (put 'make-radical 'radical
+       (lambda (c r) (tag (make-radical c r))))
+  
+  'installed!)
+
+(define (install-fractions-package)
+  ;; internal procedures
+  
+  (define mul-fractions
+    (lambda (frac1 frac2)
+      (operate frac1 frac2 numer denom)))
+
+  (define div-fractions
+    (lambda (frac1 frac2)
+      (operate frac1 frac2 denom numer)))
+
+  (define operate
+    (lambda (frac1 frac2 selectorA selectorB)
+      (make-rat (* (numer frac1) (selectorA frac2))
+		(* (denom frac1) (selectorB frac2)))))
+    
+  (define add-fractions
+    (lambda (f1 f2)
+      (compute f1 f2 +)))
+  
+  (define sub-fractions
+    (lambda (f1 f2)
+      (compute f1 f2 -)))
+  
+  (define compute
+    (lambda (frac1 frac2 op)
+      (make-rat (op (* (numer frac1) (denom frac2))
+		    (* (numer frac2) (denom frac1)))
+		(* (denom frac1) (denom frac2)))))
+
+  (define make-rat
+    (lambda (numerator denominator)
+      (let ((g (gcd numerator denominator)))
+	(cons (/ numerator g) (/ denominator g)))))
+  
+  (define numer
+    (lambda (fraction)
+      (car fraction)))
+
+  (define denom
+    (lambda (fraction)
+      (cdr fraction)))
+
+  ;; interface to the rest of the system
+  (define (tag x) (attach-tag 'fraction x))
+
+  (put 'add '(fraction fraction)
+       (lambda (x y) (tag (add-fractions x y))))
+
+  (put 'sub '(fraction fraction)
+       (lambda (x y) (tag (sub-fractions x y))))
+
+  (put 'mul '(fraction fraction)
+       (lambda (x y) (tag (mul-fractions x y))))
+
+  (put 'div '(fraction fraction)
+       (lambda (x y) (tag (div-fractions x y))))
+
+  (put 'make 'fraction
+       (lambda (n d) (tag (make-rat n d))))
+
+  'installed!)
+
+(define (install-matrix-package)
+  ;; internal procedures
+
+  (define add-matrices
+    (lambda (m1 m2)
+      (compute-matrices m1 m2 +)))
+
+  (define subtract-matrices
+    (lambda (m1 m2)
+      (compute-matrices m1 m2 -)))
+  
+  (define compute-matrices
+    (lambda (matrix1 matrix2 op)
+      (map (lambda (x y) (map op x y))
+	   matrix1
+	   matrix2)))
+
+  ;; interface to the rest of the system
+  (define tag
+    (lambda (x) (attach-tag 'matrix x)))
+
+  (put 'add '(matrix matrix)
+       (lambda (x y) (tag (add-matrices x y))))
+  
+  (put 'sub '(matrix matrix)
+       (lambda (x y) (tag (subtract-matrices x y))))
+  
+  'installed!)
+    
+  
+
+;; install packages
+(install-radical-package)
+(install-fractions-package)
+(install-matrix-package)
+
+;;; tests
+(define (check-expect check expect)
+  (if (not (equal? check expect))
+      (begin (display "check-expect failed; got: ")
+	     (display check)
+	     (display "; expected: ")
+	     (display expect)
+	     (newline))
+      (begin (display "test passed!")
+	     (newline))))
+    
+
+(define (tag x) (attach-tag 'radical x))
+(define (tag-fraction x) (attach-tag 'fraction x))
+(define (tag-matrix x) (attach-tag 'matrix x))
+
+(define (tests)
+
+  ;;; testing the generic `add` procedure
+  
+  ;; radicals
+  (define radical1 (make-radical 2 3))
+  (check-expect (add radical1 radical1) (tag (cons 4 3)))
+
+  (define rad2 (make-radical 3 4))
+  (define rad3 (make-radical 2 4))
+  (check-expect (sub rad2 rad3) (tag 2))
+
+  (check-expect (mul rad2 rad3) (tag 24))
+
+  ;; fractions
+  (define f1 (make-rat 4 6))
+  (define f2 (make-rat 2 3))
+  (define f3 (make-rat 2 6))
+
+  (check-expect (add f1 f2) (tag-fraction (cons 4 3)))
+
+  (check-expect (sub f1 f3) (tag-fraction (cons 1 3)))
+
+  (check-expect (mul f1 f2) (tag-fraction (cons 4 9)))
+
+  (check-expect (div f1 f2) (tag-fraction (cons 1 1)))
+
+  ;; matrices
+  (define matrix1 (tag-matrix (list (list 2 3 4) (list 3 4 5))))
+  (define matrix2 (tag-matrix (list (list 10 15 20) (list 7 8 9))))
+
+  (check-expect (add matrix1 matrix1) (tag-matrix (list (list 4 6 8) (list 6 8 10))))
+  (check-expect (sub matrix2 matrix1) (tag-matrix (list (list 8 12 16) (list 4 4 4))))
+
+  'done)
+
+(tests)
---
+++ help.scm
@@ -0,0 +1,38 @@
+;; these procedures are not made by me; they are procedures from the book
+;; "Structure and Interpretation of Computer Programs"; thanks.
+
+
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+          (apply proc (map contents args))
+          (if (= (length args) 2)
+              (let ((type1 (car type-tags))
+                    (type2 (cadr type-tags))
+                    (a1 (car args))
+                    (a2 (cadr args)))
+                (let ((t1->t2 (get-coercion type1 type2))
+                      (t2->t1 (get-coercion type2 type1)))
+                  (cond (t1->t2
+                         (apply-generic op (t1->t2 a1) a2))
+                        (t2->t1
+                         (apply-generic op a1 (t2->t1 a2)))
+                        (else
+                         (error "No method for these types"
+                                (list op type-tags))))))
+              (error "No method for these types"
+                     (list op type-tags)))))))
+
+(define (attach-tag type-tag contents)
+  (cons type-tag contents))
+
+(define (type-tag datum)
+  (if (pair? datum)
+      (car datum)
+      (error "Bad tagged datum -- TYPE-TAG" datum)))
+
+(define (contents datum)
+  (if (pair? datum)
+      (cdr datum)
+      (error "Bad tagged datum -- CONTNETS" datum)))
